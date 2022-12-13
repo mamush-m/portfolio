3351063fddcf1d766a5059f2f76a74b32645df4c
mamush-m <svrmamush@gmail.com>
2022-12-13 16:40:54 -0500

    trimmed

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './home.css';
 // import binary from '../../resources/videos/Binary_Code.mp4';
+import binary1 from '../../resources/videos/trimmed.mp4';
 
 export const Home = () => {
     return (
         <div id="homeBody">
             <div id="binary">
-                {/* <video src={binary} loop autoPlay></video> */}
+                <video src={binary1} loop autoPlay></video>
             </div>
         </div>
     )
