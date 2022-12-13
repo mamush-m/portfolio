3a264fe30670d49ae1d42a92d4722aa36f8375e6
mamush-m <svrmamush@gmail.com>
2022-12-13 13:55:19 -0500

    binary 2

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
 import './home.css';
-// import binary from '../../resources/videos/Binary_Code.mp4';
+import binary from '../../resources/videos/Binary_Code.mp4';
 
 export const Home = () => {
     return (
         <div id="homeBody">
             <div id="binary">
-                {/* <video src={binary} loop autoPlay></video> */}
+                <video src={binary} loop autoPlay></video>
             </div>
         </div>
     )
